6866e21318fd5bb9cdf1a93f85faa853c529bdb8
syo572 <syo572@icloud.com>
2021-05-09 20:42:28 +0900

    投稿機能の追加完了

--- app.js
+++ app.js
@@ -2,37 +2,56 @@ const express = require("express");
 const app = express();
 const mysql = require('mysql');
 
+//ここ要確認
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 //viewエンジンをejsであることを設定
 app.set("view engine", "ejs");
 
 //ログイン画面
-app.get('/login',(req,res) => {
+app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
 //認証処理
-app.post('/login',(req,res) => {
+app.post('/login', (req, res) => {
   const email = req.body;
   console.log(email);
 });
 //トップ画面
 app.get('/', (req, res) => {
-  res.render('top.ejs');
-  });
+  connection.query(
+    'SELECT * FROM article',
+    (error, result) => {
+      res.render('top.ejs', { article: result });
+    }
+  );
+});
+//トップ画面処理
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO article (text) VALUES (?)',
+    [req.body.add],
+    (error, result) => {
+      res.redirect('/');
+    }
+  );
+});
 
 //sqlを接続
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'konatu572',
-    database: 'node_forum'
+  host: 'localhost',
+  user: 'root',
+  password: 'konatu572',
+  database: 'node_forum'
 });
 //sql接続できないときのエラー処理
 connection.connect((err) => {
-    if(err){
-        console.log('error connecting:'+err.stack);
-        return;
-    }
-    console.log('success');
+  if (err) {
+    console.log('error connecting:' + err.stack);
+    return;
+  }
+  console.log('success');
 });
 
 
